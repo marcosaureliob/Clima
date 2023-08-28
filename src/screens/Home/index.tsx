0a2ab3bb6ba0f462d716478a0bc43010c8be06c1
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-08-27 23:18:12 -0300

    Inicio de projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
